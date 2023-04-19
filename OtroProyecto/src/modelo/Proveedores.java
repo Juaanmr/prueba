0560fa344a0923b5681c5d76c227d4eda68a7e1e
Juan <Juan@DESKTOP-IS0TO9O>
2023-04-19 20:47:46 +0200

    comit con atributos de clientes

--- OtroProyecto/src/modelo/Proveedores.java
+++ OtroProyecto/src/modelo/Proveedores.java
@@ -1,5 +1,19 @@
 package modelo;
 
 public class Proveedores {
-
+	int idCliente;
+	String nombreCliente;
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+	
 }
